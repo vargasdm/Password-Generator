0bea5d1709b96557b397854fa9fbdc8f851c703b
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-10-29 13:08:15 -0400

    changed generatedPassword concatination to generatedPassword += chosenIndex;

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ['`', "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", '<', '>', '?', "+", "-", ',', "=", ":", '/', '.', "[", "]", "|", "{", "}"];
+var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "<", ">", "?", "+", "-", ",", "=", ":", "/", '.', "[", "]", "|", "{", "}"];
 var chosenArrays = [];
 var generatedPassword = "";
 
@@ -136,7 +136,7 @@ function writePassword() {
 for (var i = 0; i < userLengthChoice; i++) {
   var index = Math.floor(Math.random() * userLengthChoice);
   var chosenIndex = chosenArrays[index];
-  generatedPassword = generatedPassword.concat(chosenIndex);
+  generatedPassword += chosenIndex;
 }
 
 console.log(generatedPassword)
