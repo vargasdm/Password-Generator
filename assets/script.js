e78b2486d65f4bfed04f0039d703d1c0522f2ed0
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-10-29 14:05:27 -0400

    changed var index = Math.floor(Math.random() * userLengthChoice); to var index = Math.floor(Math.random() * chosenArrays.length);

--- assets/script.js
+++ assets/script.js
@@ -134,7 +134,7 @@ function writePassword() {
 //  use for loop to randomly choose index in chosenArray and store character in a new variable. loop until  is designated length. each character will be concatinated with the previous one to lengthen the password
 // take chosenArrays and randomly select an index, store in variable, concat the next chosen index tot he previouse opn and sotre the new value
 for (var i = 0; i < userLengthChoice; i++) {
-  var index = Math.floor(Math.random() * userLengthChoice);
+  var index = Math.floor(Math.random() * chosenArrays.length);
   var chosenIndex = chosenArrays[index];
   generatedPassword += chosenIndex;
 }
@@ -142,7 +142,10 @@ for (var i = 0; i < userLengthChoice; i++) {
 console.log(generatedPassword)
 
   passwordText.value = generatedPassword;
+  
   }
+
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
